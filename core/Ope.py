726763b903af9557a018b0d9cc0dadfa9d56a343
Ardus Petus <laurent.de.richemont@gmail.com>
2026-07-31 08:12:46 +0200

    Commentaires

--- core/Ope.py
+++ core/Ope.py
@@ -9,12 +9,12 @@ class Ope:
 
     DATE_EOF = _date(9999, 12, 31)
 
-    def __str__(self):
+    def __str__(self)->str:
         return f"[{self.date.strftime('%d/%m/%Y')} / {self.lib[:50]:<50} / {self.montant}]"
 
-    def isEOF(self):
+    def isEOF(self)->bool:
         return self.date == Ope.DATE_EOF
 
     @staticmethod
-    def EOF():
+    def EOF()->Ope:
         return Ope(Ope.DATE_EOF, '', 0)
